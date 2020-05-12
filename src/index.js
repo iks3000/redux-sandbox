309fdcf8548a81224186253556b4b05d233807b4
Stanislav <stanislav.khomyn@skylum.com>
2020-05-11 17:57:38 -0700

    bindActionCreators

--- src/index.js
+++ src/index.js
@@ -1,26 +1,33 @@
-import { createStore } from 'redux';
+import { createStore, bindActionCreators } from 'redux';
 import reducer from './reducer';
-import { inc, dec, rnd } from './actions';
+import * as actions from './actions';
 
 const store = createStore(reducer);
+const { dispatch } = store;
+
+// const bindActionCreator = (creator, dispatch) => (...args) => {
+//   dispatch(creator(...args))
+// }
+
+const { inc, dec, rnd } =
+  bindActionCreators(actions, dispatch);
+
+// const decDispatch = bindActionCreator(dec, dispatch);
+// const rndDispatch = bindActionCreator(rnd, dispatch);
 
 document
   .getElementById('inc')
-  .addEventListener('click', () => {
-    store.dispatch(inc());
-  })
+  .addEventListener('click', inc);
 
 document
   .getElementById('dec')
-  .addEventListener('click', () => {
-    store.dispatch(dec());
-  })
+  .addEventListener('click', dec);
 
 document
   .getElementById('rnd')
   .addEventListener('click', () => {
     const payload = Math.floor(Math.random() * 10);
-    store.dispatch(rnd(payload));
+    rnd(payload);
   })
 
 const update = () => {
