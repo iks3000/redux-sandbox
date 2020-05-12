144fd738023d06b16ac7561584d78d14568fc3e5
Stanislav <stanislav.khomyn@skylum.com>
2020-05-12 12:59:22 -0700

    Updates

---
+++ precache-manifest.acd32ce1c65fd452b6db7c3b923027fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "130bd6546c3310441058db96a10e63f2",
+    "url": "/redux-sendbox/index.html"
+  },
+  {
+    "revision": "9c0860f8c82516807412",
+    "url": "/redux-sendbox/static/js/2.792c47dd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-sendbox/static/js/2.792c47dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec8479d42ca71682af95",
+    "url": "/redux-sendbox/static/js/main.55a9e7ec.chunk.js"
+  },
+  {
+    "revision": "b2da8919aed958cf50f1",
+    "url": "/redux-sendbox/static/js/runtime-main.264ac4fa.js"
+  }
+]);
