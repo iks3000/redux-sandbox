37ff5397a87d42955f17be31e43032123b9aad49
Stanislav <stanislav.khomyn@skylum.com>
2020-05-12 13:15:23 -0700

    Updates

---
+++ precache-manifest.455872b9f53d49015ed1ea9ffce59a56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e4901c1670e78142621c58cfe9f444d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c0860f8c82516807412",
+    "url": "/static/js/2.792c47dd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.792c47dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec8479d42ca71682af95",
+    "url": "/static/js/main.55a9e7ec.chunk.js"
+  },
+  {
+    "revision": "af9c663d2500a0bd416c",
+    "url": "/static/js/runtime-main.02c4e70e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/redux-sendbox/precache-manifest.acd32ce1c65fd452b6db7c3b923027fc.js"
+  "/precache-manifest.455872b9f53d49015ed1ea9ffce59a56.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/redux-sendbox/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
